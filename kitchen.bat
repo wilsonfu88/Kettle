9f410badac65b795910d607bed1a20f6d0691bb9
wilson.fu <wilson.fu@foxmail.com>
2018-01-19 11:33:09 +0800

    转换修改

--- kitchen.bat
+++ kitchen.bat
@@ -1,3 +1,3 @@
 cd D:\books\Database\ETL\data-integration
 d:
-Kitchen /file  E:\temp\20180119\kettle\data_ex_job.kjb /level:Basic>>E:\temp\20180119\kettle\log\etl.log
+Kitchen /file  E:\temp\20180119\kettle\data_ex_job.kjb
